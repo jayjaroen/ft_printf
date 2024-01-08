9b1985d2565f027044d6c419b9ea4bf30a41ac6c
Jay <jay.ji.jaroen@gmail.com>
2024-01-08 22:21:33 +0700

    Initialize the project

---
+++ ft_printf.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjaroens <jjaroens@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 15:22:41 by jjaroens          #+#    #+#             */
+/*   Updated: 2024/01/04 22:19:59 by jjaroens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+//The number of char read in the standard output
+//The pointer of the argument to the list
+//Macro in varadic function
+int	ft_printf(const char *args, ...)
+{
+    va_list ap; //argument pointer --> standard naming for va_list // already a pointer
+    va_start(ap, args);
+
+
+
+
+}
+
+int main(void)
+{
+
+}
---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjaroens <jjaroens@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/03 14:54:15 by jjaroens          #+#    #+#             */
+/*   Updated: 2024/01/03 22:17:32 by jjaroens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+# define FT_PRINTF
+
+// variadic function && macro
+#include <stdarg.h>
+int	ft_printf(const char *args, ...);
+#endif
